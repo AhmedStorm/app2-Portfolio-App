a3c9d1d9a6b805fe793467599380c04c8d735bd7
Ahmed Barhoma <63800760+AhmedStorm@users.noreply.github.com>
2023-11-25 11:57:25 +0200

    Update home.py

--- home.py
+++ home.py
@@ -10,7 +10,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.image("images/photo.png")
 with col2:
-    st.title("Ardit Sulce")
+    st.title("Ahmed Barhoma")
     st.info("""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
     commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
